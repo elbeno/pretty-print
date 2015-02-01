962b441c4aa6244cc89ef106ef664062e74673b6
Ben Deane <ben@elbeno.com>
2015-01-31 23:55:57 -0800

    Support most types

--- src/include/prettyprint.h
+++ src/include/prettyprint.h
@@ -1,12 +1,25 @@
 #pragma once
 
 #include <algorithm>
+#include <functional>
 #include <iostream>
 #include <type_traits>
 #include <utility>
-#include <vector>
 
 // -----------------------------------------------------------------------------
+// A pretty-printer for (pretty much) any type.
+//
+// * If operator<< is available, it will be used.
+// * If the type is not printable (like a function) we will attempt to
+//   characterize it anyway using type_traits and print something like
+//   <function>.
+// * Containers (iterable, with begin() and end()) get printed with customizable
+//   openers, closers and separators. (The default is { , })
+// * Pairs are printed (like,this) as are tuples.
+// * Enum values and enum class values are printed as ints.
+
+// -----------------------------------------------------------------------------
+// SFINAE member/functionality detection
 template <typename...>
 using void_t = void;
 
@@ -18,7 +31,14 @@ using void_t = void;
   template <typename T>                                                 \
   struct has_##name<T, void_t<name##_t<T>>> : public std::true_type {};
 
+// -----------------------------------------------------------------------------
+// Does the type support operator<< ?
 SFINAE_DETECT(operator_output, std::cout << std::declval<T>())
+
+struct has_operator_output_tag {};
+
+// -----------------------------------------------------------------------------
+// Is the type iterable (has begin() and end())?
 SFINAE_DETECT(begin, std::declval<T>().begin())
 SFINAE_DETECT(end, std::declval<T>().end())
 
@@ -27,14 +47,97 @@ using is_iterable = typename std::conditional<
   has_begin<T>::value && has_end<T>::value,
   std::true_type, std::false_type>::type;
 
+struct is_iterable_tag {};
+
 // -----------------------------------------------------------------------------
-template <typename T, bool, bool>
-struct stringifier_select;
+// Is the type a pair or tuple?
+template <typename T>
+struct is_pair : public std::false_type {};
+template <typename T, typename U>
+struct is_pair<std::pair<T, U>> : public std::true_type {};
+
+struct is_pair_tag {};
 
 template <typename T>
-using stringifier = stringifier_select<T,
-                                       has_operator_output<T>::value,
-                                       is_iterable<T>::value>;
+struct is_tuple : public std::false_type {};
+template <typename... Ts>
+struct is_tuple<std::tuple<Ts...>> : public std::true_type {};
+
+struct is_tuple_tag {};
+
+// -----------------------------------------------------------------------------
+// Is the type a callable of some kind?
+
+SFINAE_DETECT(call_operator, &T::operator())
+
+template <typename T>
+struct is_std_function : public std::false_type {};
+template <typename T>
+struct is_std_function<std::function<T>> : public std::true_type {};
+
+struct is_std_function_tag {};
+struct is_function_tag {};
+struct is_callable_tag {};
+
+// -----------------------------------------------------------------------------
+// Is the type an enum or enum class?
+
+struct is_enum_tag {};
+
+// -----------------------------------------------------------------------------
+// Is the type something else we want to distinguish?
+
+struct is_union_tag {};
+struct is_class_tag {};
+struct is_nullptr_tag {};
+
+// -----------------------------------------------------------------------------
+template <typename T, typename TAG>
+struct stringifier_select;
+
+// The way we want to treat a type, in preference order. Objects with operator()
+// are tricky because evidently lambdas can be used with operator<<, but we want
+// them to show as a callable object. This means if you have an object with
+// operator() and operator<< defined, the operator<< won't be used.
+
+template <typename T>
+using stringifier_tag = std::conditional_t<
+  std::is_null_pointer<T>::value,
+  is_nullptr_tag,
+  std::conditional_t<
+    is_std_function<std::remove_reference_t<T>>::value,
+    is_std_function_tag,
+    std::conditional_t<
+      std::is_function<std::remove_reference_t<T>>::value,
+      is_function_tag,
+      std::conditional_t<
+        has_call_operator<T>::value,
+        is_callable_tag,
+        std::conditional_t<
+          std::is_enum<T>::value,
+          is_enum_tag,
+          std::conditional_t<
+            has_operator_output<T>::value,
+            has_operator_output_tag,
+            std::conditional_t<
+              is_iterable<T>::value,
+              is_iterable_tag,
+              std::conditional_t<
+                is_pair<T>::value,
+                is_pair_tag,
+                std::conditional_t<
+                  is_tuple<T>::value,
+                  is_tuple_tag,
+                  std::conditional_t<
+                    std::is_union<T>::value,
+                    is_union_tag,
+                    std::conditional_t<
+                      std::is_class<T>::value,
+                      is_class_tag,
+                      void>>>>>>>>>>>;
+
+template <typename T>
+using stringifier = stringifier_select<T, stringifier_tag<T>>;
 
 template <typename T>
 std::ostream& operator<<(std::ostream& s, const stringifier<T>& t)
@@ -50,7 +153,7 @@ stringifier<T> prettyprint(T&& t)
 
 // -----------------------------------------------------------------------------
 // default: not stringifiable
-template <typename T, bool, bool>
+template <typename T, typename TAG>
 struct stringifier_select
 {
   explicit stringifier_select(T&&) {}
@@ -63,8 +166,8 @@ struct stringifier_select
 
 // -----------------------------------------------------------------------------
 // If T has an output operator, use it
-template <typename T, bool b>
-struct stringifier_select<T, true, b>
+template <typename T>
+struct stringifier_select<T, has_operator_output_tag>
 {
   explicit stringifier_select(const T& t) : m_t(t) {}
 
@@ -77,7 +180,22 @@ struct stringifier_select<T, true, b>
 };
 
 // -----------------------------------------------------------------------------
-// If t is iterable, do that, with customization points for
+// Specialize for bool: do boolalpha explicitly so as not to affect stream state
+template <>
+struct stringifier_select<bool, has_operator_output_tag>
+{
+  explicit stringifier_select(bool t) : m_t(t) {}
+
+  std::ostream& output(std::ostream& s) const
+  {
+    return s << (m_t ? "true" : "false");
+  }
+
+  bool m_t;
+};
+
+// -----------------------------------------------------------------------------
+// If t is iterable, show that, with customization points for
 // opener/closer/separator
 
 template <typename T>
@@ -99,22 +217,119 @@ struct iterable_separator
 };
 
 template <typename T>
-struct stringifier_select<T, false, true>
+struct stringifier_select<T, is_iterable_tag>
 {
   explicit stringifier_select(const T& t) : m_t(t) {}
 
   std::ostream& output(std::ostream& s) const
   {
-    s << iterable_opener<T>::value;
+    s << iterable_opener<decay_t<T>>::value;
     auto b = m_t.begin();
     auto e = m_t.end();
     if (e != b)
       s << prettyprint(*b);
     std::for_each(++b, e,
-                  [&s] (auto& e) { s << iterable_separator<T>::value
+                  [&s] (auto& e) { s << iterable_separator<decay_t<T>>::value
                                      << prettyprint(e); });
-    return s << iterable_closer<T>::value;
+    return s << iterable_closer<decay_t<T>>::value;
   }
 
   const T& m_t;
 };
+
+// -----------------------------------------------------------------------------
+#define SS_SIMPLE_SPEC(TAG, NAME)               \
+  template <typename T>                         \
+  struct stringifier_select<T, TAG>             \
+  {                                             \
+    explicit stringifier_select(T&&) {}         \
+    std::ostream& output(std::ostream& s) const \
+    {                                           \
+      return s << #NAME;                        \
+    }                                           \
+};
+
+// -----------------------------------------------------------------------------
+// If T is a function, show that
+
+SS_SIMPLE_SPEC(is_callable_tag, <callable object>)
+SS_SIMPLE_SPEC(is_function_tag, <function>)
+SS_SIMPLE_SPEC(is_std_function_tag, <std::function>)
+
+// -----------------------------------------------------------------------------
+// If T is a regular object, show that
+
+SS_SIMPLE_SPEC(is_class_tag, <object>)
+SS_SIMPLE_SPEC(is_union_tag, <union>)
+
+// -----------------------------------------------------------------------------
+// If T is a nullptr, show that
+
+SS_SIMPLE_SPEC(is_nullptr_tag, nullptr)
+
+// -----------------------------------------------------------------------------
+// If T is an enum or enum class, show that
+template <typename T>
+struct stringifier_select<T, is_enum_tag>
+{
+  explicit stringifier_select(const T t) : m_t(t) {}
+
+  std::ostream& output(std::ostream& s) const
+  {
+    return s << static_cast<std::underlying_type_t<T>>(m_t);
+  }
+
+  T m_t;
+};
+
+// -----------------------------------------------------------------------------
+// If T is a pair, show that
+template <typename T>
+struct stringifier_select<T, is_pair_tag>
+{
+  explicit stringifier_select(const T& t) : m_t(t) {}
+
+  std::ostream& output(std::ostream& s) const
+  {
+    return s << '(' << prettyprint(m_t.first)
+             << ',' << prettyprint(m_t.second) << ')';
+  }
+
+  const T& m_t;
+};
+
+// -----------------------------------------------------------------------------
+// If t is a tuple, show that
+template <typename F, typename...Ts, std::size_t...Is>
+void for_each_in_tuple(const std::tuple<Ts...>& t, F f,
+                       std::index_sequence<Is...>)
+{
+  (void)(int[]) { 0, (f(std::get<Is>(t), Is), 0)... };
+}
+
+template <typename F, typename...Ts>
+void for_each_in_tuple(const std::tuple<Ts...>& t, F f)
+{
+  for_each_in_tuple(t, f, std::make_index_sequence<sizeof...(Ts)>());
+}
+
+template <typename T>
+struct stringifier_select<T, is_tuple_tag>
+{
+  explicit stringifier_select(const T& t) : m_t(t) {}
+
+  std::ostream& output(std::ostream& s) const
+  {
+    s << '(';
+    for_each_in_tuple(m_t,
+                      [&s] (auto& e, size_t i)
+                      { if (i > 0) s << ',';
+                        s << prettyprint(e); });
+    return s << ')';
+  }
+
+  const T& m_t;
+};
+
+#undef SFINAE_DETECT
+#undef SS_SIMPLE_SPEC
--- src/test/main.cpp
+++ src/test/main.cpp
@@ -1,10 +1,72 @@
+#include <deque>
+#include <iostream>
+#include <vector>
+using namespace std;
+
 #include "prettyprint.h"
 
-#include <iostream>
-using namespace std;
+struct Foo {};
+
+struct Bar
+{
+  void operator()() {};
+};
+
+void foobar()
+{
+}
+
+union U {};
+
+enum Quux
+{
+  FOO,
+  BAR,
+  BAZ
+};
+
+enum class Garply
+{
+  FOO,
+  BAR,
+  BAZ
+};
+
+template <typename T>
+struct iterable_opener<deque<T>>
+{
+  constexpr static const char* value = ">";
+};
+
+template <typename T>
+struct iterable_closer<deque<T>>
+{
+  constexpr static const char* value = ">";
+};
 
 int main(int argc, char* argv[])
 {
-  cout << "Hello, world!" << endl;
+  vector<int> v{1,2,3};
+  cout << prettyprint(v) << endl; // {1,2,3}
+  deque<int> d{1,2,3};
+  cout << prettyprint(d) << endl; // >1,2,3>
+
+  cout << prettyprint(true) << endl; // true
+  cout << prettyprint(BAR) << endl; // 1
+  cout << prettyprint(Garply::BAZ) << endl; // 2
+  cout << prettyprint(nullptr) << endl; // nullptr
+  cout << prettyprint("Hello, world!") << endl; // Hello, world!
+  cout << prettyprint(Foo()) << endl; // <object>
+  cout << prettyprint(U()) << endl; // <union>
+  cout << prettyprint(make_pair(1,2)) << endl; // (1,2)
+  cout << prettyprint(make_tuple("Hello", 42)) << endl; // (Hello, 42)
+  cout << prettyprint(Bar()) << endl; // <callable object>
+
+  std::function<void()> f = [](){};
+  cout << prettyprint(f) << endl; // <std::function>
+
+  cout << prettyprint([](){}) << endl; // <callable object>
+  cout << prettyprint(foobar) << endl; // <function>
+
   return 0;
 }
